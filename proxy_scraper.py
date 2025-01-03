ab9cd468591eb477617ba6bdba94385f7c66a2b9
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-03 22:43:06 +0100

    Add check proxy with session

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -4,7 +4,38 @@ import threading
 
 from bs4 import BeautifulSoup
 import httpx
-import requests
+
+from JobSpy.src.jobspy.scrapers.utils import (
+    create_session,
+)
+
+headers_initial = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9",
+    "priority": "u=0, i",
+    "referer": "https://www.google.com/",
+    "sec-ch-prefers-color-scheme": "dark",
+    "sec-ch-ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+    "sec-ch-ua-arch": '"arm"',
+    "sec-ch-ua-bitness": '"64"',
+    "sec-ch-ua-form-factors": '"Desktop"',
+    "sec-ch-ua-full-version": '"130.0.6723.58"',
+    "sec-ch-ua-full-version-list": '"Chromium";v="130.0.6723.58", "Google Chrome";v="130.0.6723.58", "Not?A_Brand";v="99.0.0.0"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": '""',
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-ch-ua-platform-version": '"15.0.1"',
+    "sec-ch-ua-wow64": "?0",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "x-browser-channel": "stable",
+    "x-browser-copyright": "Copyright 2024 Google LLC. All rights reserved.",
+    "x-browser-year": "2024",
+}
 
 
 class Scraper:
@@ -143,16 +174,14 @@ class GitHubScraper(Scraper):
 
 
 def is_https_working(proxy):
-    proxies = {
-        "http": 'http://{}'.format(proxy),
-        # "https": 'http://{}'.format(proxy)
-    }
     try:
-        response = requests.get('https://google.com', proxies=proxies, timeout=5)
-        if response.status_code == 200 and '<title>Google</title>' in response.text and 'name="q"' in response.text:
+        params = {"q": "jobs in Texas USA", "udm": "8"}
+        response = create_session(proxies=proxy, is_tls=False, ca_cert=None, has_retry=False, clear_cookies=True,
+                                  delay=1).get(
+            'https://google.com', headers=headers_initial, timeout=5, params=params)
+        if response.status_code == 200:
             return True
     except Exception as e:
-        print(e)
         return False
     return False
 
