1a1600206e2ff02ee85ce5df4bdbbf1ed6675fe4
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-13 09:28:51 +0100

    Refactor database service

--- db/database_service.py
+++ db/database_service.py
@@ -8,8 +8,9 @@ class UserManager:
 
     def add_user(self, email, position, location, job_type):
         user = User(email=email, position=position, location=location, job_type=job_type)
-        self.session.add(user)
-        self.session.commit()
+        if not self.user_exists(email, position, location):
+            self.session.add(user)
+            self.session.commit()
 
     def delete_user(self, email, position, location):
         user = self.session.query(User).filter_by(email=email, position=position, location=location).first()
@@ -28,11 +29,11 @@ class UserEmailManager:
     def __init__(self):
         self.session = Session()
 
-    def add_sent_email(self, email, job_url, position):
-        user = SentEmail(email=email, job_url=job_url, position=position)
+    def add_sent_email(self, email, job_url, position, location):
+        user = SentEmail(email=email, job_url=job_url, position=position, location=location)
         self.session.add(user)
         self.session.commit()
 
-    def is_sent(self, email, job_url, position):
+    def is_sent(self, email, job_url, position, location):
         return self.session.query(SentEmail).filter_by(email=email, job_url=job_url, position=position,
                                                        location=location).count() > 0
