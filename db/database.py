ab919480ef467db14fef992b7ba98e55d62329f4
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-19 14:04:56 +0100

    Refactor database file to prevent too many open connections

--- db/database.py
+++ db/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import sessionmaker, scoped_session
 from db.models import Base
 
 DATABASE_URL = "sqlite:///job_finder.db"
@@ -8,4 +7,6 @@ DATABASE_URL = "sqlite:///job_finder.db"
 engine = create_engine(DATABASE_URL, echo=True)
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
+SessionFactory = sessionmaker(bind=engine)
+
+Session = scoped_session(SessionFactory)
--- db/database_service.py
+++ db/database_service.py
@@ -4,37 +4,43 @@ from db.models import User, SentEmail
 
 class UserManager:
     def __init__(self):
-        self.session = Session()
+        pass
 
     def add_user(self, email, position, location, job_type):
-        user = User(email=email, position=position, location=location, job_type=job_type)
-        if not self.user_exists(email, position, location):
-            self.session.add(user)
-            self.session.commit()
+        with Session() as session:
+            user = User(email=email, position=position, location=location, job_type=job_type)
+            if not self.user_exists(email, position, location):
+                session.add(user)
+                session.commit()
 
     def delete_user(self, email, position, location):
-        user = self.user_exists(email=email, position=position, location=location)
-        if user:
-            self.session.delete(user)
-            self.session.commit()
+        with Session() as session:
+            user = self.user_exists(email=email, position=position, location=location)
+            if user:
+                session.delete(user)
+                session.commit()
 
     def user_exists(self, email, position, location):
-        return (self.session.query(User)
-                .filter_by(email=email, position=position, location=location).one_or_none())
+        with Session() as session:
+            return (session.query(User)
+                    .filter_by(email=email, position=position, location=location).one_or_none())
 
     def get_all_users(self):
-        return self.session.query(User).all()
+        with Session() as session:
+            return session.query(User).all()
 
 
 class UserEmailManager:
     def __init__(self):
-        self.session = Session()
+        pass
 
     def add_sent_email(self, email, job_url, position, location):
-        user = SentEmail(email=email, job_url=job_url, position=position, location=location)
-        self.session.add(user)
-        self.session.commit()
+        with Session() as session:
+            user = SentEmail(email=email, job_url=job_url, position=position, location=location)
+            session.add(user)
+            session.commit()
 
     def is_sent(self, email, job_url, position, location):
-        return self.session.query(SentEmail).filter_by(email=email, job_url=job_url, position=position,
-                                                       location=location).count() > 0
+        with Session() as session:
+            return session.query(SentEmail).filter_by(email=email, job_url=job_url, position=position,
+                                                      location=location).count() > 0
