bf47429515ed5adc425bf5e4dd62824c59e3c838
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-20 12:43:40 +0100

    Refactor credential.py

--- credential.py
+++ credential.py
@@ -2,27 +2,31 @@ import os
 
 
 class Credential:
-    def __init__(self):
-        pass
-
-    def get_email_address(self):
+    @staticmethod
+    def get_email_address():
         return os.environ.get("email_address")
 
-    def get_email_password(self):
+    @staticmethod
+    def get_email_password():
         return os.environ.get("email_password")
 
-    def get_google_api(self):
+    @staticmethod
+    def get_google_api():
         return os.environ.get("google_api_key")
 
-    def get_db_name(self):
+    @staticmethod
+    def get_db_name():
         return os.environ.get("db_name")
 
-    def get_db_password(self):
+    @staticmethod
+    def get_db_password():
         return os.environ.get("db_password")
 
-    def get_db_username(self):
+    @staticmethod
+    def get_db_username():
         return os.environ.get("db_username")
 
-    def get_conn_uri(self):
+    @staticmethod
+    def get_db_uri():
         return "postgresql://%s:%s@127.0.0.1:5432/%s" % (
-        self.get_db_username(), self.get_db_password(), self.get_db_name())
+            Credential.get_db_username(), Credential.get_db_password(), Credential.get_db_name())
