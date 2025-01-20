73b089b49479ab60ee49a7f790eba97eee5504f7
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-20 17:02:02 +0100

    Add db_url to credentials

--- credential.py
+++ credential.py
@@ -27,6 +27,19 @@ class Credential:
         return os.environ.get("db_username")
 
     @staticmethod
+    def get_db_host():
+        return os.environ.get('db_port', '127.0.0.1')
+
+    @staticmethod
+    def get_db_port():
+        return os.environ.get('db_port', '5432')
+
+    @staticmethod
     def get_db_uri():
-        return "postgresql://%s:%s@127.0.0.1:5432/%s" % (
-            Credential.get_db_username(), Credential.get_db_password(), Credential.get_db_name())
+        return os.environ.get("db_url", "postgresql://%s:%s@%s:%s/%s" % (
+            Credential.get_db_username(),
+            Credential.get_db_password(),
+            Credential.get_db_host(),
+            Credential.get_db_port(),
+            Credential.get_db_name()
+        ))
