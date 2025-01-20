a951e62c4a6ecdc821ff3c6974456020965cee2f
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2025-01-20 11:59:13 +0100

    Add db credentials

--- credential.py
+++ credential.py
@@ -13,3 +13,16 @@ class Credential:
 
     def get_google_api(self):
         return os.environ.get("google_api_key")
+
+    def get_db_name(self):
+        return os.environ.get("db_name")
+
+    def get_db_password(self):
+        return os.environ.get("db_password")
+
+    def get_db_username(self):
+        return os.environ.get("db_username")
+
+    def get_conn_uri(self):
+        return "postgresql://%s:%s@127.0.0.1:5432/%s" % (
+        self.get_db_username(), self.get_db_password(), self.get_db_name())
